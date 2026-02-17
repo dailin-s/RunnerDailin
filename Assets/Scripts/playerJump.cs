92485bb09e272d392710ca2231345b8d14bdd59d
dailin <dailinsiggers@gmail.com>
2026-02-16 20:57:26 -0600

    updated player jump
    made it so that it syncs with button

--- Assets/Scripts/playerJump.cs
+++ Assets/Scripts/playerJump.cs
@@ -5,11 +5,13 @@ public class playerJump : MonoBehaviour
 
     public float jumpForce = 7f;
     private Rigidbody2D rb;
+    private AudioSource audioSource;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -18,5 +20,7 @@ public class playerJump : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Space)){
             rb.linearVelocity = Vector2.up * jumpForce;
         }
+
+        audioSource.Play();
     }
 }
